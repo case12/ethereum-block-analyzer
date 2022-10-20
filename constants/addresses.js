c5c2492724380fe0271b7dba3f80b60f99ef39da
Adam Hauer <ajhauer@gmail.com>
2022-10-20 21:06:11 +0200

    Listen for new blocks, print out most interacted with addresses

---
+++ constants/addresses.js
@@ -0,0 +1,36 @@
+const addresses = [
+  {
+    name: "Uniswap V2: Router 2",
+    address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+  },
+  {
+    name: "Uniswap V3: Router 2",
+    address: "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45",
+  },
+  {
+    name: "Tether USDT Stablecoin",
+    address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+  },
+  {
+    name: "USDC",
+    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+  },
+  {
+    name: "Town Star Token",
+    address: "0xc36cF0cFcb5d905B8B513860dB0CFE63F6Cf9F5c",
+  },
+  {
+    name: "Seaport 1.1",
+    address: "0x00000000006c3852cbEf3e08E8dF289169EdE581",
+  },
+  {
+    name: "MemoryPageFactRegistry (StarkEx???)",
+    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+  },
+  {
+    name: "There (Bot?)",
+    address: "0x6D88EB0fEb4CF93cb0D89f22DEEecd627CB5F318",
+  },
+];
+
+module.exports = { ADDRESS_LIST: addresses };
---
+++ scripts/monitor.js
@@ -0,0 +1,63 @@
+const hre = require("hardhat");
+const moment = require("moment");
+const _ = require("lodash");
+const { ADDRESS_LIST } = require("../constants/addresses");
+
+const provider = hre.ethers.getDefaultProvider();
+
+async function main() {
+  provider.on("block", async (blockNumber) => {
+    const block = await provider.getBlockWithTransactions(blockNumber);
+
+    console.log("\n\n***************************");
+    console.log(`Block Number: ${block.number}`);
+    console.log(`Block Hash: ${block.hash}`);
+    console.log(`Time: ${moment(block.timestamp * 1000).format("LL LTS")}`);
+    console.log(`Number of transactions: ${block.transactions.length}`);
+
+    const initialTransactionMap = _.reduce(
+      ADDRESS_LIST,
+      (acc, address) => {
+        return {
+          ...acc,
+          [address.address]: { name: address.name, count: 0 },
+        };
+      },
+      {}
+    );
+
+    const transactionsMap = _.reduce(
+      block.transactions,
+      (result, transaction) => ({
+        ...result,
+        [transaction.to]: result[transaction.to]
+          ? {
+              ...result[transaction.to],
+              address: transaction.to,
+              count: result[transaction.to].count + 1,
+            }
+          : { address: transaction.to, count: 1 },
+      }),
+      initialTransactionMap
+    );
+
+    const sortedTransactions = _.sortBy(
+      _.values(transactionsMap),
+      "count"
+    ).reverse();
+
+    console.log("Showing top 10 addresses interacted with last block");
+    console.log(sortedTransactions.slice(0, 10));
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+[
+  { name: "Unknown", count: 30 },
+  { name: "Uniswap Contract", count: 5 },
+  { name: "Sushiswap Contract", count: 2 },
+];
